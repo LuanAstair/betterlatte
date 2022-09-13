7b06b69083cc782077ae3a7829c790300100ccc8
LuanAstair <johnlhenardsantiago@gmail.com>
2022-09-13 10:32:10 +0800

    commit

--- ims_django/settings.py
+++ ims_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rehm$-g0&_c-nzb54b7xp-pcnehad63vd4b-4zlwh=o839g1f@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
