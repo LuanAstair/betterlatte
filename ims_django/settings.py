e57a93c33140fdc04e9f93c5c9ca6f2b0ee0f8d0
LuanAstair <johnlhenardsantiago@gmail.com>
2022-09-13 13:45:17 +0800

    Update settings.py

--- ims_django/settings.py
+++ ims_django/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -33,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1','betterlatte.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
